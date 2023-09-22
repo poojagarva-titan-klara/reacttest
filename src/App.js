97251f5d6311fcf1406bf1916a791dd385189b1f
poojagarva2810 <pooja@avdevs.com>
2023-09-22 15:50:28 +0530

    tags changed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Testing github actions</div>;
+  return (
+    <div className="App">
+      <h2>Testing Github Actions</h2>
+    </div>
+  );
 }
 
 export default App;
